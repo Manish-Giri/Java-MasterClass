b3be2f1dcedaa85c442c71e4dc30417abcba06bf
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-10 22:47:13 -0400

    Commit lecture on classes intro

---
+++ src/section7/Lecture37.java
@@ -0,0 +1,13 @@
+package section7;
+
+/**
+ * Created by manishgiri on 5/10/17.
+ */
+public class Lecture37 {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+}
