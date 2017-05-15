b0c77fdcde3118abdb920dd7d19fda4bac37e7e5
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-14 23:30:05 -0400

    Composition - Dimensions and Resolution classes

---
+++ src/section8/Dimensions.java
@@ -0,0 +1,29 @@
+package section8;
+
+/**
+ * Created by manishgiri on 5/14/17.
+ */
+public class Dimensions {
+
+    private int height;
+    private int width;
+    private int depth;
+
+    public Dimensions(int height, int width, int depth) {
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+}
---
+++ src/section8/Resolution.java
@@ -0,0 +1,23 @@
+package section8;
+
+/**
+ * Created by manishgiri on 5/14/17.
+ */
+public class Resolution {
+
+    private int height;
+    private int width;
+
+    public Resolution(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
