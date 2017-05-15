a6c9faffbf7db0e9226e7c00ddd0a3311e65cc1c
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-14 23:20:40 -0400

    Composition - Motherboard class

---
+++ src/section8/Motherboard.java
@@ -0,0 +1,64 @@
+package section8;
+
+/**
+ * Created by manishgiri on 5/14/17.
+ */
+public class Motherboard {
+    private String model;
+    private String manufacturer;
+    private String bios;
+    private int ramSlots;
+    private int cardSlots;
+
+    public Motherboard(String model, String manufacturer, String bios, int ramSlots, int cardSlots) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.bios = bios;
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getBios() {
+        return bios;
+    }
+
+    public void setBios(String bios) {
+        this.bios = bios;
+    }
+
+    public int getRamSlots() {
+        return ramSlots;
+    }
+
+    public void setRamSlots(int ramSlots) {
+        this.ramSlots = ramSlots;
+    }
+
+    public int getCardSlots() {
+        return cardSlots;
+    }
+
+    public void setCardSlots(int cardSlots) {
+        this.cardSlots = cardSlots;
+    }
+
+    public void loadProgram(String programName) {
+        System.out.println("Program " + programName + " is loading.");
+    }
+}
