b9a6865f4bfb62d99e3b2f2f2d6ee5eb84aa6653
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-14 23:44:12 -0400

    Composition - Complete Main class

--- src/section8/MainL46.java
+++ src/section8/MainL46.java
@@ -5,14 +5,18 @@ package section8;
  */
 public class MainL46 {
 
-    Dimensions dimensions = new Dimensions(20, 20, 5);
-    Case aCase = new Case("2208", "Dell", "240", dimensions);
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case aCase = new Case("2208", "Dell", "240", dimensions);
 
-    Monitor monitor = new Monitor("27 inch beast", "Acer", 27, new Resolution(2540, 1440));
+        Monitor aMonitor = new Monitor("27 inch beast", "Acer", 27, new Resolution(2540, 1440));
 
-    Motherboard motherboard = new Motherboard("BN-200", "Asus", "4", 6, 4);
+        Motherboard aMotherboard = new Motherboard("BN-200", "Asus", "4", 6, 4);
 
-    PC thePC = new PC(aCase, monitor, motherboard);
+        PC thePC = new PC(aCase, aMonitor, aMotherboard);
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 98");
+        thePC.getTheCase().pressPowerButton();
+    }
 
-    
 }
