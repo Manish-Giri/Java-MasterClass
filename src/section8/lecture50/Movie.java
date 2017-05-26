6742508cb460a986285876baec39a9f7bf6a49a2
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-25 23:46:33 -0400

    Polymorphism - Create Movie Class

---
+++ src/section8/lecture50/Movie.java
@@ -0,0 +1,17 @@
+package section8.lecture50;
+
+/**
+ * Created by manishgiri on 5/25/17.
+ */
+ class Movie {
+
+    private String name;
+
+    Movie(String name) {
+        this.name = name;
+    }
+
+    String plot() {
+        return "No plot here";
+    }
+}
