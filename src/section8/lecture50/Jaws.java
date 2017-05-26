08aec0afb735d2fe1aadbc18dcfab98cc3b1d6bc
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-25 23:47:07 -0400

    Polymorphism - Create Jaws Class

---
+++ src/section8/lecture50/Jaws.java
@@ -0,0 +1,16 @@
+package section8.lecture50;
+
+/**
+ * Created by manishgiri on 5/25/17.
+ */
+class Jaws extends Movie {
+
+    Jaws() {
+        super("Jaws");
+    }
+
+    String plot() {
+        return "A shark eats lots of people";
+    }
+
+}
