c24ee92ee8e389368ae029dbec8621bdc4580e26
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-26 01:09:08 -0400

    ArrayChallenge - Bubble Sort

---
+++ src/section9/ArrayChallenge.java
@@ -0,0 +1,47 @@
+package section9;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by manishgiri on 5/26/17.
+ */
+public class ArrayChallenge {
+
+    private static int[] bubbleSortArray(int[] array) {
+        int[] result = Arrays.copyOf(array, array.length);
+        int temp = 0;
+        // sort
+        for(int i = 0; i < result.length-1; i++) {
+            for (int j = 0; j < (result.length - i)- 1; j++) {
+                if(result[j] > result[j+1]) {
+                    temp = result[j+1];
+                    result[j+1] = result[j];
+                    result[j] = temp;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static int[] getIntegers(int num) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[num];
+        System.out.println("Enter " + num + " integers:");
+        for(int i = 0; i < num; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        return numbers;
+    }
+
+    public static void main(String[] args) {
+        int size = 5;
+
+        int[] numbers = getIntegers(size);
+        System.out.println("Entered: " + Arrays.toString(numbers));
+
+        int[] sortedArr = bubbleSortArray(numbers);
+        System.out.println("Sorted: " + Arrays.toString(sortedArr));
+    }
+}
