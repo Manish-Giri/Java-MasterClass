a1c35dd81c51a7be16176e9c2c07906fb6fe31da
MANISH-GIRI <manish.giri.me@gmail.com>
2017-05-26 23:34:03 -0400

    ArrayList - part 1, add, print

---
+++ src/section9/L57.java
@@ -0,0 +1,22 @@
+package section9;
+
+import java.util.ArrayList;
+
+/**
+ * Created by manishgiri on 5/26/17.
+ */
+public class L57 {
+
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your list.");
+        for(int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i+1) + ". " + groceryList.get(i));
+        }
+    }
+}
